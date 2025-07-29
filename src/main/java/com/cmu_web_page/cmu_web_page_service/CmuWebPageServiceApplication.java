3cdb8fee0f3424da050fa55d3f7531f1e76e0524
ZhuoranCool <zhw195@pitt.edu>
2025-07-29 20:02:08 +0800

    Initial commit

---
+++ src/main/java/com/cmu_web_page/cmu_web_page_service/CmuWebPageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cmu_web_page.cmu_web_page_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmuWebPageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmuWebPageServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmu_web_page/cmu_web_page_service/CmuWebPageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmu_web_page.cmu_web_page_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmuWebPageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
